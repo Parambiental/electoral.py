fd309fa7b13186998b64a6d1d09bfa4b5a4df119
Parambiental <parambiental.py@gmail.com>
2025-07-07 22:17:17 -0300

    Create mockData.js

---
+++ src/data/mockData.js
@@ -0,0 +1,7 @@
+export const electors = [
+  { cedula: "1234567", nombre: "Juan Pérez", telefono: "0981123456", local: "Escuela Herrera", mesa: "12", orden: "025", lat: -25.28, lng: -57.57 },
+  { cedula: "2345678", nombre: "María López", telefono: "0982345678", local: "Colegio San Cristóbal", mesa: "18", orden: "147", lat: -25.29, lng: -57.58 },
+  { cedula: "3456789", nombre: "Carlos Romero", telefono: "0983456789", local: "Esc. Básica N°45", mesa: "09", orden: "098", lat: -25.30, lng: -57.59 },
+  { cedula: "4567890", nombre: "Ana González", telefono: "0984567890", local: "Centro Educativo Herrera", mesa: "21", orden: "311", lat: -25.27, lng: -57.55 },
+  { cedula: "5678901", nombre: "Pedro Duarte", telefono: "0985678901", local: "Colegio Nacional", mesa: "14", orden: "177", lat: -25.26, lng: -57.54 }
+];
